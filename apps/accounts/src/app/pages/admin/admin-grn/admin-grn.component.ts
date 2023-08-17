5cc849fdbf221c602c0e4edddefbb23ed296d2d8
29Niro <29niro@gmail.com>
2023-08-17 23:17:29 +0530

    update constructor to retrive date properly & add styling to card

--- apps/accounts/src/app/pages/admin/admin-grn/admin-grn.component.ts
+++ apps/accounts/src/app/pages/admin/admin-grn/admin-grn.component.ts
@@ -6,8 +6,13 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
-import { Firestore, addDoc, collectionData, collection } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import {
+  Firestore,
+  addDoc,
+  collectionData,
+  collection,
+} from '@angular/fire/firestore';
+import { Observable, map } from 'rxjs';
 interface GRN {
   date: Date;
   customer: {
@@ -53,26 +58,38 @@ export class AdminGrnComponent {
 
   grnCollection = collection(this.firestore, 'grn');
 
+  // constructor() {
+  //   this.grns$ = collectionData(this.grnCollection) as Observable<GRN[]>;
+  //   // this.grns$.subscribe((d)=>{
+  //   //   console.log(d);
+  //   // })
+  // }
+
+  //this constructor for get date properly
   constructor() {
-    this.grns$ = collectionData(this.grnCollection) as Observable<GRN[]>;
-    // this.grns$.subscribe((d)=>{
-    //   console.log(d);
-    // })
+    this.grns$ = collectionData(this.grnCollection).pipe(
+      map((grns: any[]) => {
+        return grns.map((grn: any) => ({
+          ...grn,
+          date: grn.date.toDate(),
+        }));
+      })
+    );
   }
 
-  async onSave(){
+  async onSave() {
     const date = new Date(this.dateCtrl.value);
     // console.log(this.grnFormGroup.value);
     const toSave = {
       ...this.grnFormGroup.value,
       date,
-    }
-    await addDoc(this.grnCollection, toSave)
+    };
+    await addDoc(this.grnCollection, toSave);
   }
 
   // same as above
   // async onSave() {
-  //   await addDoc(this.grnCollection, 
+  //   await addDoc(this.grnCollection,
   //     {
   //       date: new Date(this.dateCtrl.value),
   //       customer: {
